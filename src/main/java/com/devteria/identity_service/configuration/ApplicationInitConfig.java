2f467ee268012f8cd3d1013a4c763aa6f0cb46bb
Sotatek-Minhta2 <minh.ta2@sotatek.com>
2025-03-07 17:04:19 +0700

    #20

--- src/main/java/com/devteria/identity_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/devteria/identity_service/configuration/ApplicationInitConfig.java
@@ -3,6 +3,7 @@ package com.devteria.identity_service.configuration;
 import java.util.HashSet;
 
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +26,9 @@ public class ApplicationInitConfig {
   PasswordEncoder passwordEncoder;
 
   @Bean
+  @ConditionalOnProperty(prefix = "spring", value = "datasource.driverClassName", havingValue = "com.mysql.cj.jdbc.Driver")
   ApplicationRunner applicationRunner(UserRepository userRepository) {
+    log.info("Init application.....");
     return args -> {
       if (!userRepository.existsByUsername("admin")) {
         var roles = new HashSet<String>();
